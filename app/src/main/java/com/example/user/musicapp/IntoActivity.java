24a3f59d1334c7e358b681353e8ba5881c42a77e
Kathure <lizkathure@gmail.com>
2016-07-21 14:26:08 +0300

    added fragments

--- app/src/main/java/com/example/user/musicapp/IntoActivity.java
+++ app/src/main/java/com/example/user/musicapp/IntoActivity.java
@@ -1,5 +1,9 @@
 package com.example.user.musicapp;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +13,48 @@ public class IntoActivity  extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_into);
+
+        addSlide(SampleSlide.newInstance(R.layout.first_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.second_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.third_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.fourth_fragment));
+
+
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#26547C"));
+        setSeparatorColor(Color.parseColor("#26547c"));
+
+        // Hide Skip/Done button.
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+
+
     }
-}
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+        Intent intent = new Intent(IntoActivity.this,MainActivity.class);
+        startActivity(intent);
+
+
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+       Intent intent = new Intent(IntoActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+    }
+
